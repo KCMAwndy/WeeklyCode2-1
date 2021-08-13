55e1b54d9284a4d5164ec8652dbfab9813c2487f
Attaphon <plianattaphon@gmail.com>
2021-08-13 12:35:28 +0700

    Add more description about the point lies in quandrant

--- WeeklyCode2-1/WeeklyCode2-1.cpp
+++ WeeklyCode2-1/WeeklyCode2-1.cpp
@@ -6,10 +6,10 @@ int main() {
 	scanf("%d", &x);
 	printf("Enter Y : ");
 	scanf("%d", &y);
-	if (x == 0 || y == 0)	printf("NO Q");
-	else if (x > 0 && y > 0)	printf("Q1");
-	else if (x < 0 && y>0)	printf("Q2");
-	else if (x < 0 && y < 0)	printf("Q3");
-	else if (x > 0 && y < 0)	printf("Q4");
+	if (x == 0 || y == 0)		printf("This point doesn't lies in the quandrant.");
+	else if (x > 0 && y > 0)	printf("This point lies in the 1st quandrant.");
+	else if (x < 0 && y>0)		printf("This point lies in the 2nd quandrant.");
+	else if (x < 0 && y < 0)	printf("This point lies in the 3rd quandrant.");
+	else if (x > 0 && y < 0)	printf("This point lies in the 4th quandrant.");
 	return 0;
 }
